e8b972de3438dacf2da3bf342690fdd7184e9146
Heran76 <heran76@gmail.com>
2024-10-07 11:09:31 +0200

    tipos_de_datos

---
+++ Programacion_js/04_tipos_de_datos/main.js
@@ -0,0 +1,22 @@
+/*
+Tipos de Datos
+---------------
+String o cadena de texto;
+Number o número entero
+Number; numeo decimal (float o double)
+boleano: verdadero o falso;
+undefined valor sin defenir;
+NULL valor null no hay nada en la variable intencionado.
+Array
+Obejetos.
+*/
+
+let frase = "Hola Mundo"; // Strig o cadena de texto;
+let anio = 2027;          // Numero entero // int(integer);
+let interes = 2.7;        // Numero decimal / float / double;
+let mayorEdad = true;     //boleano;
+let vacia;                //undefined;
+let nula = null;          //null;
+
+let fruta =["fresa", "Sandia", "naranja"];  //array o arreglo se define con corchete;
+let heroe ={nombre: "Batman", universo:"DC"} // obejetos se abre y cierran con llaves y van clave : valor
