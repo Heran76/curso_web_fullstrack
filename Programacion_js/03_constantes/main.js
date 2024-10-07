5b6ce09305fde3679208bfb7491f563e099f6456
Heran76 <heran76@gmail.com>
2024-10-07 10:52:55 +0200

    constantes

--- Programacion_js/03_constantes/main.js
+++ Programacion_js/03_constantes/main.js
@@ -1,1 +1,10 @@
 //constantes
+/*una variable cuyo contenido nunca cambia. si se intenta cambiar 
+el sistema da un error */
+
+let nombre = "Antonio" //variable
+const dni = "26493546-F" //Consante numero que nunca cambia.
+
+//const dni = 5 esto no se puede hacer
+
+console.log(nombre, dni);
