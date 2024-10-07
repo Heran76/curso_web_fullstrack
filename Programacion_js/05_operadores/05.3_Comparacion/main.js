566601ee030aada04d03e170341cf1ad567751a4
Heran76 <heran76@gmail.com>
2024-10-07 12:15:05 +0200

    comparacion

---
+++ Programacion_js/05_operadores/05.3_Comparacion/main.js
@@ -0,0 +1,11 @@
+//Comparacion
+
+let numerazo = 31;
+console.log(numerazo == 31); // comparando si es igual no true
+console.log(numerazo === '31'); // nos da folse por no es igual estrizo;
+console.log(numerazo != 32 ); // es diferente
+console.log(numerazo !== "31" ); // diferente estricto;
+console.log(numerazo > 55) // mayor que >
+console.log(numerazo < 55) // menor que <
+console.log(numerazo >= 55) // mayor o igual que >=
+console.log(numerazo <= 55) // menor o igual que <=
