13033847bac986825941dd90ad672cb326e12d33
Heran76 <heran76@gmail.com>
2024-10-07 12:22:28 +0200

    cremento y decremento

---
+++ Programacion_js/05_operadores/05.6_CrementoYdecremento/main.js
@@ -0,0 +1,17 @@
+// OPERADOR DECREMENTO Y CREMENTO
+
+let cifra = 1200
+
+// cifra = 1200 +1 = cifra++;
+//cifra = 1200 -1 = cifra--;
+
+cifra++;
+cifra++;
+cifra++;
+console.log(cifra);
+
+cifra--;
+cifra--;
+cifra--;
+cifra--;
+console.log(cifra);
