8373771f5c34e20096785f3c6dc5e649ec25a147
Heran76 <heran76@gmail.com>
2024-10-09 12:21:38 +0200

    && ||

--- Programacion_js/07_condicionales/condicionales_logica/main.js
+++ Programacion_js/07_condicionales/condicionales_logica/main.js
@@ -1,7 +1,7 @@
 //ejemplo 5
 //Condicionales logica
 
-let year = 2030;
+let year =2027;
 
 if(year >= 2000 && year <= 2030){
     console.log("Estamos en la era moderna");
@@ -9,4 +9,12 @@ if(year >= 2000 && year <= 2030){
     console.log("Estamos en la era Post Moderna");
 }else{
     console.log("Estas en la era antigua!!");
+}
+
+//Ejercicio 6.
+
+if(year == 2007 || year == 2017 || year == 2027 || year == 2037){
+    console.log("el año acaba en 7!!!");
+}else{
+    console.log("Año desconocido!!");
 }
