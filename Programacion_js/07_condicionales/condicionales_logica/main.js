2102498b0c321bd919732c71a7cdef4a6028c396
Heran76 <heran76@gmail.com>
2024-10-09 12:13:01 +0200

    condicionales_logicos

---
+++ Programacion_js/07_condicionales/condicionales_logica/main.js
@@ -0,0 +1,12 @@
+//ejemplo 5
+//Condicionales logica
+
+let year = 2030;
+
+if(year >= 2000 && year <= 2030){
+    console.log("Estamos en la era moderna");
+}else if(year > 2030){
+    console.log("Estamos en la era Post Moderna");
+}else{
+    console.log("Estas en la era antigua!!");
+}
