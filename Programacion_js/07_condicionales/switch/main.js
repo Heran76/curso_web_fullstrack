f622d20e80678f3a5aa090446fbb94ad93e22b09
Heran76 <heran76@gmail.com>
2024-10-11 11:25:51 +0200

    switch

---
+++ Programacion_js/07_condicionales/switch/main.js
@@ -0,0 +1,22 @@
+/*
+condiciona Switch // case
+*/
+
+let desayuno = 2;
+
+switch(desayuno){
+    case 1 :
+        console.log("has elegido el desayuno numero 1");
+    break;
+    
+    case 2 :
+        console.log("has elegido el desayuno numero 2");
+    break;
+    
+    case 3 :
+        console.log("has elegiodo el desayuno numero 3");
+    break;
+    
+        default:
+        console.log("No has selccionado ningún desayuno")    
+}
