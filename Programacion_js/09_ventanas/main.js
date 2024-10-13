b8b94cdf876eea444e4ca65a438f7599da072444
Heran76 <heran76@gmail.com>
2024-10-13 21:54:20 +0200

    ventas y alertar

---
+++ Programacion_js/09_ventanas/main.js
@@ -0,0 +1,14 @@
+//Alertas
+
+let mensaje = "Hola soy Antonio";
+console.log(mensaje);
+
+// confirmacion
+
+let pregunta = confirm("Estas seguro que quieres continuar");
+console.log(pregunta);
+
+//Prompts (ingreso de datos)
+
+let edad = prompt("¿Qué edad tienes?",0);
+console.log(parseInt(edad));
