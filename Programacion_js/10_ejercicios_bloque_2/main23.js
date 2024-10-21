5418d8fcc72882b87f5feb783bac1efbbed7ad2c
Heran76 <heran76@gmail.com>
2024-10-21 12:32:38 +0200

    Ejercicio23

---
+++ Programacion_js/10_ejercicios_bloque_2/main23.js
@@ -0,0 +1,12 @@
+/*
+Ejercicio 23
+
+Tenemos que despegar un cohete al espacio;
+y necesitamos una cuenta regresiva de 10.
+hazlo con un bucle for.
+*/
+alert("Comienza la cuenta regresiva")
+for(let cuenta = 10; cuenta > 0; cuenta--){
+    alert(cuenta)
+}
+ alert("Let's go!")
