0e95305b5cf16d047578e2bd5f6722062addfec5
Heran76 <heran76@gmail.com>
2024-10-24 11:02:37 +0200

    Arrays fundamentos

---
+++ Programacion_js/13_Arrays/arrays_fundamentales/main.js
@@ -0,0 +1,21 @@
+/*
+Array : serie de variables con un mismo nombre e indice, y 
+empieza por el indice 0
+*/
+
+let nombre = "Antoni Heredia";
+let nombres = ["Marcos", "Antonio","Jose","Paco"];
+
+//constructor array
+let peliculas = new Array("Iron Man","Gran torino","Mamamia")
+
+
+
+console.log(nombres);
+console.log(nombres.length);
+console.log(peliculas);
+console.log(peliculas[2]); //acceso al indice
+
+
+
+
