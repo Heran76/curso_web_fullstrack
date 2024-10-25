d26ff2fee2091931432ad0120c52083330b362d2
Heran76 <heran76@gmail.com>
2024-10-25 13:04:29 +0200

    recorrer array con bucle for

---
+++ Programacion_js/13_Arrays/Arrays_Avanzados/main.js
@@ -0,0 +1,17 @@
+/*
+Recorrer Array con bucle for
+*/
+
+let fremwork = ["React","Angular","Astro","Vue","Nex.js"]
+
+for(let i = 0; i < fremwork.length; i++ ){
+    console.log(fremwork[i]);
+}
+
+//Mostrar por pantalla un poco desfasado.
+
+
+for(let i = 0; i < fremwork.length; i++ ){
+    document.write("<li>"+fremwork[i]+"</li>");
+}
+
