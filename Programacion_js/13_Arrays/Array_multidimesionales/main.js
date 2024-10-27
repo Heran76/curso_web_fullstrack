9d06fadd615e637deef2e1f58941ef8fd95f098c
Heran76 <heran76@gmail.com>
2024-10-27 10:55:44 +0100

    Array Multidimensional

---
+++ Programacion_js/13_Arrays/Array_multidimesionales/main.js
@@ -0,0 +1,14 @@
+/*
+Array multidimensionaels
+*/
+
+let categorias = ["Lucha", "Deportes","Acción","Sandbox","Carreras"];
+let titulos = ["Tekken", "Fifa", "max Payne 3","GTA 6", "Nfs Unbound"];
+
+let videojuegos = [categorias, titulos];
+console.log(videojuegos);
+
+console.log(videojuegos[1][2]);
+console.log(videojuegos[0][4]);
+
+
