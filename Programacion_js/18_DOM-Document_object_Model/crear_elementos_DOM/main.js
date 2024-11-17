4a23c70d2f00208d21ca79214ac17cce31d75de4
Heran76 <heran76@gmail.com>
2024-11-17 20:58:54 +0100

    Crear elementos DOM

---
+++ Programacion_js/18_DOM-Document_object_Model/crear_elementos_DOM/main.js
@@ -0,0 +1,22 @@
+//Crear elementos DOM
+
+const tarjeta = document.querySelector("#tarjeta");
+console.log(tarjeta)
+
+//agregar clase
+tarjeta.classList.add("destacar");
+
+//quitar clase
+tarjeta.classList.remove("remarcar");
+
+//togle quita o pon segun si exite o no
+tarjeta.classList.toggle("small")
+
+//añadir elemento
+
+const titular = document.createElement("h2");
+titular.textContent="Hola caracola";
+tarjeta.append(titular);
+
+tarjeta.insertBefore(titular, tarjeta.firstChild);
+
