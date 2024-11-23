9f9fa5e585c4730b59d0b4b3386ac4e3e2c39d55
Heran76 <heran76@gmail.com>
2024-11-23 11:26:19 +0100

    Objeto Windows

---
+++ Programacion_js/19_BOM/controlar_ventana/main.js
@@ -0,0 +1,10 @@
+//OBJETO WINDOW
+console.log(window);
+console.log(window.innerHeight);
+console.log(window.innerWidth);
+
+console.log(window.outerHeight, window.outerWidth);
+
+console.log(windo.outerHeight, window.outerWidth);
+
+console.log(window.closed, window.document, window.navigator.userAgent);
