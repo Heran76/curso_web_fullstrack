6abaf6c54c32f892737fed887c1aa3d40e5fe08a
Heran76 <heran76@gmail.com>
2024-12-09 16:27:27 +0100

    Eventos keyup

--- 20-Eventos/eventos_teclado/main.js
+++ 20-Eventos/eventos_teclado/main.js
@@ -1,5 +1,12 @@
+//Eventos de teclado 
+//Keydown
 let campoTexto = document.querySelector("#campotexto");
 campoTexto.addEventListener("keydown", (event)=>{
     console.log("has pulsado la tecla"+event.key);
+})
+
+//keyup
+campoTexto.addEventListener("keyup",(event)=>{
+console.warn("Has soltado la tecla: "+event.key);
 })
 
