5bf708629aaf84b1993a5ba048c6b06ec10bdeb5
Heran76 <heran76@gmail.com>
2024-12-09 17:59:19 +0100

    Eventos teclado

--- 20-Eventos/eventos-formulario/main.js
+++ 20-Eventos/eventos-formulario/main.js
@@ -1,1 +1,10 @@
 //Eventos teclado.
+
+let superformulario = document.querySelector("#superformulario");
+superformulario.addEventListener("submit",(event)=>{
+    event.preventDefault();
+    let nombre = document.querySelector("#nombre").value;
+    let correo = document.querySelector("#email").value; 
+
+    alert(`El nombre es : ${nombre} \n El correo es : ${correo}`)
+})
