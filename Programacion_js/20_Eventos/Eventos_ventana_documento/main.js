ff138d501eb7f113a0f9d5699578625a26bfdd28
Heran76 <heran76@gmail.com>
2024-12-11 20:30:10 +0100

    eventos ventano y documento

--- Programacion_js/20_Eventos/Eventos_ventana_documento/main.js
+++ Programacion_js/20_Eventos/Eventos_ventana_documento/main.js
@@ -3,3 +3,7 @@ document.addEventListener("DOMContentLoaded", () =>{
     //aqui hago todo mi código de js
     console.log("TODO EL DOM ESTA CARGADO !!");
 })
+
+windows.addEventListener("load",() => {
+    console.log("Toda la web está cargada, incluida los recurso !!**");
+})
