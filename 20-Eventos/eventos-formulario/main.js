11568dc17d1f8bfbee571e11c90b8fe28200799b
Heran76 <heran76@gmail.com>
2024-12-11 18:55:02 +0100

    Eventos input

--- 20-Eventos/eventos-formulario/main.js
+++ 20-Eventos/eventos-formulario/main.js
@@ -1,6 +1,4 @@
 //Eventos teclado.
-
-
 let superformulario = document.querySelector("#superformulario");
 superformulario.addEventListener("submit",(event)=>{
     event.preventDefault();
@@ -9,3 +7,11 @@ superformulario.addEventListener("submit",(event)=>{
 
     alert(`El nombre es : ${nombre} \n El correo es : ${correo}`)
 })
+
+//Eventos Input
+
+let inputNombre = document.querySelector("#nombre");
+inputNombre.addEventListener("input", ()=>{
+    console.log("El usuario a escrito "+inputNombre.value)
+})
+
