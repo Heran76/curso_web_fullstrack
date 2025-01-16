4e70d93434c9ea262d28d5b7e760b51aeb7238ff
Heran76 <heran76@gmail.com>
2025-01-16 17:21:37 +0100

    JSON

--- Programacion_js/22_objetos_literales_json/22-json/main.js
+++ Programacion_js/22_objetos_literales_json/22-json/main.js
@@ -1,15 +1,16 @@
-const { json } = require("react-router-dom");
+
 
 //Json javascript pbejct notations
 let palaDePadel = {
-    nombre : "Metalbone",
-    marca : "Adidas",
-    anio : 2022,
-    peso : 347,
-    forma : "Diamante",
-    dureza : "Media"
-
+    nombre: "Metalbone",
+    marca: "Adidas",
+    anio: 2022,
+    peso: 347,
+    forma: "Diamante",
+    dureza: "Media"
 };
+
+
 let palaDePadelJSON = JSON.stringify(palaDePadel)
 console.log(palaDePadel)
 console.log(palaDePadelJSON)
@@ -18,4 +19,10 @@ console.log(palaDePadelJSON)
 
 let objetoConvertido = JSON.parse(palaDePadelJSON);
 console.log("*****[JSON convertido]******")
-console.log(objetoConvertido)
+console.log(objetoConvertido);
+
+//recorrer un objeto 
+let caja = document.querySelector("#caja");
+for(let clave in palaDePadel){
+    caja.innerHTML += `<p>${clave}: ${palaDePadel[clave]}</p>`;
+}
