63e43eda64cff4c477c8791fe32734c71a84f719
Heran76 <heran76@gmail.com>
2025-01-16 16:56:12 +0100

    JSON

---
+++ Programacion_js/22_objetos_literales_json/22-json/main.js
@@ -0,0 +1,21 @@
+const { json } = require("react-router-dom");
+
+//Json javascript pbejct notations
+let palaDePadel = {
+    nombre : "Metalbone",
+    marca : "Adidas",
+    anio : 2022,
+    peso : 347,
+    forma : "Diamante",
+    dureza : "Media"
+
+};
+let palaDePadelJSON = JSON.stringify(palaDePadel)
+console.log(palaDePadel)
+console.log(palaDePadelJSON)
+
+//Convertir un objeto JSON a un objeto literal
+
+let objetoConvertido = JSON.parse(palaDePadelJSON);
+console.log("*****[JSON convertido]******")
+console.log(objetoConvertido)
