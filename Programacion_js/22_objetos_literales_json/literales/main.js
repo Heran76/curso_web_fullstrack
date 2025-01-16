c632560895ceafd8702cf9738067746a2161ab44
Heran76 <heran76@gmail.com>
2025-01-16 14:46:41 +0100

    Objetos literal

--- Programacion_js/22_objetos_literales_json/literales/main.js
+++ Programacion_js/22_objetos_literales_json/literales/main.js
@@ -1,4 +1,6 @@
-
+//objetos literales
+//Es una estructura de datos que agurpa propiedades y métodos
+//bajo el mismo nombre
 
 
 
@@ -7,10 +9,16 @@ let pelicula = {
     anio : 1984,
     genero : "Ciencia ficción",
     prota : "Arnold Schwarzenegger",
+    elenco : ["Arnold","Linda","otros"],
     descripcion : function(){  //obejto this
         return `${this.titulo} es una pelicula
                 de ${this.prota}, hecha en el año ${this.anio}
                `
+    },
+    mostrarElenco:  ()=>{
+        pelicula.elenco.forEach(nombre =>{
+            console.log(nombre);
+        })
     }
 
 }
@@ -34,3 +42,12 @@ console.log(pelicula)
 //utilizar this para acceder a los valores
 console.log("*****[this]*****")
 console.log(pelicula.descripcion())
+
+//utilizar array
+console.log("****[Array]*****")
+console.log(pelicula.elenco)
+
+//eliminar un objeto delete
+delete pelicula.genero;
+console.log("*****[delete//genero*****]");
+console.log(pelicula)
