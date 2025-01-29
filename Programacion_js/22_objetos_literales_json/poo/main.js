26fe550282b105668ebfdec6ac66273fdccb57b6
Heran76 <heran76@gmail.com>
2025-01-29 14:06:26 +0100

    Set /get

--- Programacion_js/22_objetos_literales_json/poo/main.js
+++ Programacion_js/22_objetos_literales_json/poo/main.js
@@ -1,11 +1,11 @@
 //programacion orienado a objetos - Poo
 
 class Gato {
-    constructor(){
+    constructor(nombre,raza,color){
         //Propiedades
-        this.nombre = "Bella";
-        this.raza = "Michu";
-        this.color= "Marron";
+        this.nombre = nombre;
+        this.raza = raza;
+        this.color= color;
     }
     //Métodos (acciones)
         saltar(){
@@ -14,8 +14,21 @@ class Gato {
         maullar(){
             console.log(`${this.nombre} ha saltado`)
         }
+// metodo get y set
+        getRaza(){
+            return this.raza
+        }
+        setRaza(raza){
+             this.raza = raza
+        }
 }
-let myGato = new Gato();
-console.log(myGato);
+
+
+
+let myGato = new Gato("Michu","Persa","marron");
+console.log(myGato.getRaza());
+myGato.setRaza("Callejero")
+console.log(`la raza de ${myGato.nombre} es ${myGato.getRaza()}`)
+
 myGato.maullar();
 myGato.saltar();
