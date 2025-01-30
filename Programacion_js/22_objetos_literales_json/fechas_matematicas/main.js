22d4b150824fce7f34ee5b02047be42958118a53
Heran76 <heran76@gmail.com>
2025-01-30 16:28:33 +0100

    matematicas

--- Programacion_js/22_objetos_literales_json/fechas_matematicas/main.js
+++ Programacion_js/22_objetos_literales_json/fechas_matematicas/main.js
@@ -5,3 +5,18 @@ console.log(fechaActual)
 console.log(fechaActual.getMonth()+1);
 console.log(fechaActual.getFullYear());
 console.log(fechaActual.getDate());
+
+// Operaciones Matemáticas supercalculadora
+console.log("***** Matematicas ******")
+console.log(`el numero pi = ${Math.PI}`)
+
+//Numero Euler
+console.log(Math.E)
+// Redondeo al entero mas cercano
+console.log(Math.round(7.2)) 
+//redondear a la baja
+console.log(Math.floor(7,9))
+//redondear al alza
+console.log(Math.ceil(7.01))
+//Números aleatorios
+console.log(Math.round(Math.random()*10));
