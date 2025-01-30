a36b7996961a83c317d39376119706aa9e581fe9
Heran76 <heran76@gmail.com>
2025-01-30 16:33:19 +0100

    Matematicas

--- Programacion_js/22_objetos_literales_json/fechas_matematicas/main.js
+++ Programacion_js/22_objetos_literales_json/fechas_matematicas/main.js
@@ -20,3 +20,12 @@ console.log(Math.floor(7,9))
 console.log(Math.ceil(7.01))
 //Números aleatorios
 console.log(Math.round(Math.random()*10));
+//Valor absoluto
+console.log(Math.abs(-29-9))
+//Pontencias
+console.log(Math.pow(7,2));
+//raiz
+console.log(Math.sqrt(7))
+//minimo y maximo
+console.log(Math.max(1,7,8,9,76,42))
+console.log(Math.min(1,2,5,8))
