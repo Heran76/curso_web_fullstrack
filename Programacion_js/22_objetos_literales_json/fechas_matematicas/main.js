14d17335ca942391dc32aefe89d8859f2df21ace
Heran76 <heran76@gmail.com>
2025-01-30 14:03:59 +0100

    Fechas y matematicas

---
+++ Programacion_js/22_objetos_literales_json/fechas_matematicas/main.js
@@ -0,0 +1,7 @@
+// objeto date - fechas
+
+let fechaActual = new Date();
+console.log(fechaActual)
+console.log(fechaActual.getMonth()+1);
+console.log(fechaActual.getFullYear());
+console.log(fechaActual.getDate());
