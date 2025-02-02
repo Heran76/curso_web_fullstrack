47536f4cb14be575dc41905b39a6940ec43b7474
Heran76 <heran76@gmail.com>
2025-02-02 19:09:37 +0100

    Guardar local stroge

--- Programacion_js/23_local_storage/main.js
+++ Programacion_js/23_local_storage/main.js
@@ -9,3 +9,12 @@ if(typeof(Storage) !== "undefined"){
 localStorage.setItem("age","49")
 //sacar por el DOM el dato del local storage
 document.querySelector("#edad").textContent = localStorage.getItem("age");
+//guardar objetos en el local storage.
+
+let animal = {
+    especie : "Elefante",
+    nombre : "Dumbo",
+    color : "Gris",
+}
+
+localStorage.getItem("animal", JSON.stringify(animal))
