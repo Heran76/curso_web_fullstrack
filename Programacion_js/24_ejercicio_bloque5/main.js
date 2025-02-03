0f8927893e356522e7e3417c2880e693c528b608
Heran76 <heran76@gmail.com>
2025-02-03 14:09:42 +0100

    Ejercicio bloque 5

---
+++ Programacion_js/24_ejercicio_bloque5/main.js
@@ -0,0 +1,21 @@
+/*
+¡¡¡ Super Ejercicio de Javascript !!!
+
+Crea una apliación web en la que puedas añadir, mostrar y eliminar
+tus peliculas javoritas.
+Cada peli que guardes debe tener:
+-titulo de la pelicula
+-Fecha guardado
+-Puntuación de pipularidad ( un valor aleatorio en 1 y 100).
+
+Las peliculas deben mantenerse guardadas aunque se recarguen la página
+y se debe poder eliminar cualquiera de ellas de la lista en cualquier momento
+
+Tareeas :
+ - Ejercicio 39 : Crea el formulario necesario
+ - Ejercicio 40 : Función para guardar peliculas con los datos necesarios
+ - Ejercicio 41 : función para mostrar peliculas extraidas del localStorage
+ -Ejercicio 42 : Haz que las peliculas carguen automaiticamente al abrir la web
+ -Ejercicio 43 : Función para eliminar peliculas.
+
+*/
