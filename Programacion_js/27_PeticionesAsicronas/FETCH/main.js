c8c79db795e3a93c6095dec84582b003a41cdcfb
Heran76 <heran76@gmail.com>
2025-02-10 13:41:40 +0100

    Fetch2

--- Programacion_js/27_PeticionesAsicronas/FETCH/main.js
+++ Programacion_js/27_PeticionesAsicronas/FETCH/main.js
@@ -1,7 +1,24 @@
 //FETCH (ajax)
 //https://reqres.in/
-fetch("https://reqres.in/api/users/2")
+
+let idUser = 3;
+
+
+fetch("https://reqres.in/api/users/"+idUser)
   .then(response => response.json())
   .then(usuario =>{
     console.log(usuario.data);
+    mostrarUsuario(usuario.data)
+  })
+  .catch(error =>{
+    console.log("Error al pedir los datos del servidor", error)
   });
+
+  function mostrarUsuario(user){
+    let caja = document.querySelector("#user");
+    caja.innerHTML = `
+    <h2>${user.first_name} ${user.last_name}</h2>
+    <img src ="${user.avatar}"/>
+    
+    `;
+  }
