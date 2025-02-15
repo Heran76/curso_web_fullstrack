f37eeb2708e82edeb6dfd63b6d86e9413003f925
Heran76 <heran76@gmail.com>
2025-02-15 10:16:17 +0100

    Promesas

--- Programacion_js/27_PeticionesAsicronas/promesas/main.js
+++ Programacion_js/27_PeticionesAsicronas/promesas/main.js
@@ -1,65 +1,59 @@
-//Promesas
-
-let miPromesa = new Promise((resolve, reject)=>{
-    setTimeout(()=>{
+// Primera promesa
+let miPromesa = new Promise((resolve, reject) => {
+    setTimeout(() => {
         let correcto = true;
-    if(correcto){
-        let miObjeto = {
-            nombre : "Rider Republic",
-          return  categoria: "Conducción",
-            anio: 2021
-        };
-        
-        resolve("miObjeto")
-    }else{
-        reject("No se ha cumplido la promesa :(")
-    }
-},3500);
-    });
-    
-    
+        if (correcto) {
+            let miObjeto = {
+                nombre: "Rider Republic",
+                categoria: "Conducción",
+                anio: 2021
+            };
+            resolve(miObjeto); // Resuelve con el objeto, no con una cadena
+        } else {
+            reject("No se ha cumplido la promesa :(");
+        }
+    }, 3500);
+});
 
 miPromesa
- .then(resultado =>{
-    console.warn(resultado);
- })
- .catch(error =>{
-    console.warn("Error: ",error);
- });
- // Promesas encandenadas
- function serviPizza(){
-
-     new Promise(resolve =>{
-        setTimeout(()=>resolve("Pizza servida !!"),15000);
+    .then(resultado => {
+        console.warn(resultado); // Muestra el objeto
+    })
+    .catch(error => {
+        console.warn("Error: ", error);
     });
- }
 
- function servirHamburguesa(){
-    new Promise(resolve =>{
-        setTimeout(()=>resolve("Hamburguesa servida !!"),15000);
+// Promesas encadenadas
+function serviPizza() {
+    return new Promise(resolve => { // Añade return
+        setTimeout(() => resolve("Pizza servida !!"), 15000);
     });
-    
 }
 
- function servirSolomilloSalsaAcitunas (){
-    new Promise(resolve =>{
-        setTimeout(()=>resolve("Solomillo con salsa de aceitunas servido !!"),15000);
+function servirHamburguesa() {
+    return new Promise(resolve => { // Añade return
+        setTimeout(() => resolve("Hamburguesa servida !!"), 15000);
     });
- }
+}
 
- serviPizza()
-   .then(resultado1 => {
-    console.log(resultado1);
-    return servirHamburguesa();
-   } )
-   .then(resultado2 =>{
-    console.log(resultado2)
-    return servirSolomilloSalsaAcitunas();
+function servirSolomilloSalsaAcitunas() {
+    return new Promise(resolve => { // Añade return
+        setTimeout(() => resolve("Solomillo con salsa de aceitunas servido !!"), 15000);
+    });
+}
 
-   })
-   ,the(resultado3 =>{
-    console.log(resultado3);
-   })
-   .catch(error =>{
-    console.log("Error en las comandas ; ", error)
-   })
+serviPizza()
+    .then(resultado1 => {
+        console.log(resultado1);
+        return servirHamburguesa(); // Retorna la siguiente promesa
+    })
+    .then(resultado2 => {
+        console.log(resultado2);
+        return servirSolomilloSalsaAcitunas(); // Retorna la siguiente promesa
+    })
+    .then(resultado3 => { // Corrige el error tipográfico (,the -> .then)
+        console.log(resultado3);
+    })
+    .catch(error => {
+        console.log("Error en las comandas: ", error);
+    });
