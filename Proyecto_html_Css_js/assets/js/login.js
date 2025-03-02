80c37e68bc03d1cfeb9fa6558b8df4b2fd78aa7a
Heran76 <heran76@gmail.com>
2025-03-02 14:41:56 +0100

    Primeras

---
+++ Proyecto_html_Css_js/assets/js/login.js
@@ -0,0 +1,7 @@
+let loginForm = document.querySelector(".aside__login");
+let inputName = document.querySelector("#name");
+let inputEmail = document.querySelector("#email");
+let loginPassword = document.querySelector("#password");
+let asideData = document.querySelector(".aside__data");
+let loginName = document.querySelector(".data__name");
+let btnlogout = document.querySelector(".data__logout");
