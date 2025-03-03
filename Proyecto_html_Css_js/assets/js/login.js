0c49982434c399406caabe1733334e68667119de
Heran76 <heran76@gmail.com>
2025-03-03 14:37:38 +0100

    Login simulando con javascript

--- Proyecto_html_Css_js/assets/js/login.js
+++ Proyecto_html_Css_js/assets/js/login.js
@@ -10,14 +10,32 @@ loginForm.addEventListener("submit",()=>{
     e.preventDefault();
 
     //Recoger los valores del formulario
-
-    //Comprobar que todo está relleno
-
+    let name = inputName.value;
+    let email = input.value;
+    let password = password.value;
+   //Comprobar que todo está relleno
+   let user = {};
+   if(name && email && password){
     //Guardar los datos en un objeto
-
+    user = {name, email, password};
     //Guardar en localorage
-
+    localStorage.setItem("user", JSON.stringify(user));
     //Vaciar el formulario
 
+    loginForm.reset();
+
     //Mostrar el usuario
-})
+   getuser()
+   }
+
+});
+let getuser = ()=>{
+    let myuser = localStorage.getItem("user");
+
+    if(myuser){
+        let identity = JSON.parse(myuser);
+        loginName.innerHTML = identity.name;
+        loginForm.classList.add("aside__login--hide");
+        asideData.classList.remove("aside__data--hide");
+    }
+}
