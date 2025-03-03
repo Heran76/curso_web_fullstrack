136294af6937150b0d18802d180cee3a9c8ed985
Heran76 <heran76@gmail.com>
2025-03-03 14:09:19 +0100

    Login

--- Proyecto_html_Css_js/assets/js/login.js
+++ Proyecto_html_Css_js/assets/js/login.js
@@ -5,3 +5,19 @@ let loginPassword = document.querySelector("#password");
 let asideData = document.querySelector(".aside__data");
 let loginName = document.querySelector(".data__name");
 let btnlogout = document.querySelector(".data__logout");
+
+loginForm.addEventListener("submit",()=>{
+    e.preventDefault();
+
+    //Recoger los valores del formulario
+
+    //Comprobar que todo está relleno
+
+    //Guardar los datos en un objeto
+
+    //Guardar en localorage
+
+    //Vaciar el formulario
+
+    //Mostrar el usuario
+})
