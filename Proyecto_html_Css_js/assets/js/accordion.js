f0eb60481ef0271540489502ad45da1a2566b08b
Heran76 <heran76@gmail.com>
2025-03-17 15:21:44 +0100

    Accordion

--- Proyecto_html_Css_js/assets/js/accordion.js
+++ Proyecto_html_Css_js/assets/js/accordion.js
@@ -1,6 +1,9 @@
-let collapses = document.querySelectorAll(".accordion__colapse");
+let collapses = document.querySelectorAll(".accordion__collapse");
 
 
 collapses.forEach(collapse =>{
-    let header = collapse.querySelector(".collapse__header")
+    let header = collapse.querySelector(".collapse__header");
+    header.addEventListener("click", ()=>{
+        collapse.classList.toggle("accordion__collapse--open");
+    })
 })
