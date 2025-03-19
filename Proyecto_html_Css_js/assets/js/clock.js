f24119fd1bb6cca4d28c76db52b1a1810693c56e
Heran76 <heran76@gmail.com>
2025-03-19 14:34:49 +0100

    clock

--- Proyecto_html_Css_js/assets/js/clock.js
+++ Proyecto_html_Css_js/assets/js/clock.js
@@ -1,1 +1,20 @@
-alert("hello Wolrd and clock")
+let clockTime = document.querySelector(".clock__time");
+
+setInterval(()=>{
+
+    let myDate = new Date();
+    let hours = myDate.getHours();
+    let mins = myDate.getMinutes();
+    let seconds = myDate.getSeconds();
+
+    if(hours < 10) hours = '0' + hours;
+    if(mins < 10) mins ='0' + mins;
+    if(mins < 10) seconds= '0'+seconds;
+
+
+    clockTime.innerHTML = `${hours}:${mins}:${seconds}`;
+
+
+
+
+},1000)
