c9d4424602bc5fb51d8c3ce5dcb7326cfce35a12
Heran76 <heran76@gmail.com>
2025-06-06 17:36:53 +0200

    ContactoViernes

--- Proyecto_html_Css_js/assets/js/contacto.js
+++ Proyecto_html_Css_js/assets/js/contacto.js
@@ -1,6 +1,51 @@
-window.addEventListener("load",()=>{
+window.addEventListener("load", () => {
     const validation = new JustValidate(".main__contact");
+    let isValidate = false;
 
-
-
-})
+    validation
+     .addField("#name", [  
+     {
+        rule: "required",
+        errorMessage: "El nombre es obligatorio"
+     },
+     {
+        rule: "minLength",  
+        value: 3,
+        errorMessage: "El nombre es muy corto"
+     },
+     {
+        rule: "maxLength",  
+        value: 15,
+        errorMessage: "El nombre es demasiado largo"
+     }
+     
+     ]);
+      .addField("#surname", [  
+     {
+        rule: "required",
+        errorMessage: "Los apellidos son obligatorio"
+     },
+     {
+        rule: "minLength",  
+        value: 3,
+        errorMessage: "Los apellidos son muy corto"
+     },
+     {
+        rule: "maxLength",  
+        value: 15,
+        errorMessage: "Los apellidos son demasiado largo"
+     }
+     
+     ]);
+      .addField("#email", [  
+     {
+        rule: "required",
+        errorMessage: "El correo es obligatorio"
+     },
+     {
+        rule: "email"
+        errorMessage: "El correo no es valido"
+     } 
+     
+     ]);
+});
