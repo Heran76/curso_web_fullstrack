cce27eeeda9194f79fdc5a6cf9fbffc20cfe2647
Heran76 <heran76@gmail.com>
2025-06-25 19:33:25 +0200

    modificar el redme

--- aprendiendo_typescript/dist/04-poo.js
+++ aprendiendo_typescript/dist/04-poo.js
@@ -1,25 +1,41 @@
 "use strict";
-//Programación orienteada a obejtos (POO)
-//molde
 class Gato {
-    constructor(nombres, raza, color) {
-        this.nombres = nombres;
+    constructor(nombre, raza, color) {
+        this.nombre = nombre;
         this.raza = raza;
         this.color = color;
     }
-    //metodos(accione)
     maullar() {
-        console.log(`${this.nombres} dice:¡Miau Miau`);
+        console.log(`${this.nombre} dice: ¡Miau Miau!`); // Añadí signo de exclamación
     }
     saltar() {
-        console.log(`${this.nombres} ha saltado`);
+        console.log(`${this.nombre} ha saltado`);
+    }
+    getRaza() {
+        return this.raza;
+    }
+    setRaza(raza) {
+        this.raza = raza;
+    }
+    set guardarColor(color) {
+        this.color = color;
+    }
+    get sacarColor() {
+        return this.sacarColor;
+    }
+    static saludar() {
+        console.log("Hola colega como estas");
     }
 }
+// Creación de instancias
 let miGato = new Gato("ZASCA", "Persa", "Ocre");
-console.log(miGato.raza);
-miGato.maullar();
-miGato.saltar();
-let miGato2 = new Gato("Pepe", "romano", "gris");
-console.log(miGato2.raza);
-miGato.maullar();
-miGato.saltar();
+let miGato2 = new Gato("Pepe", "Romano", "gris");
+// Llamadas CORRECTAS (asegúrate de usar la variable correcta)
+miGato.setRaza("Persa");
+console.log(miGato.getRaza()); // Muestra "Persa"
+miGato.maullar(); // Muestra "ZASCA dice: ¡Miau Miau!"
+miGato.saltar(); // Muestra "ZASCA ha saltado"
+console.log(miGato2.getRaza()); // Muestra "romano"
+miGato2.maullar(); // Muestra "Pepe dice: ¡Miau Miau!"
+miGato2.saltar(); // Muestra "Pepe ha saltado"         // "Pepe ha saltado"
+Gato.saludar();
