80f5868aaaaf8f653b3889712f87ae94480fc39d
Heran76 <heran76@gmail.com>
2025-07-11 17:54:41 +0200

    component2

--- angular/aprendiendo-angular/src/app/app.component.ts
+++ angular/aprendiendo-angular/src/app/app.component.ts
@@ -2,10 +2,10 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { ProductoComponent } from './producto/producto.component';
 import { UsuarioComponent } from './usuario/usuario.component';
-
+import { Producto2Component } from './producto2/producto2.component';
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, ProductoComponent,UsuarioComponent],
+  imports: [RouterOutlet, ProductoComponent,UsuarioComponent,Producto2Component],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
