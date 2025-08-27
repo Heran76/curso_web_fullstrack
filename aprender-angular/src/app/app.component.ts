0c497754e9cd3ce35723cc20511bc1a5ca20c445
Heran76 <heran76@gmail.com>
2025-08-27 18:45:24 +0200

    principio componentes

--- aprender-angular/src/app/app.component.ts
+++ aprender-angular/src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import{ProductoComponent} from './producto/producto.componet'
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [ProductoComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
