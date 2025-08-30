86cfa023488409145a36e40db17f1c4a2bc19dd4
Heran76 <heran76@gmail.com>
2025-08-30 11:15:51 +0200

    first-template

--- aprender-angular/src/app/app.component.ts
+++ aprender-angular/src/app/app.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
 import{ProductoComponent} from './producto/producto.componet';
-import{VentasComponent} from './ventas/ventas.component'
-
+import{VentasComponent} from './ventas/ventas.component';
+import { PortfolioComponent } from './portfolio/portfolio.component';
 @Component({
   selector: 'app-root',
-  imports: [ProductoComponent, VentasComponent],
+  imports: [ProductoComponent, VentasComponent, PortfolioComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
