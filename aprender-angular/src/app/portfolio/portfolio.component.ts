2abb129233f35222dac980df48d9c3bfc48c74fc
Heran76 <heran76@gmail.com>
2025-09-01 20:15:33 +0200

    lunes1

--- aprender-angular/src/app/portfolio/portfolio.component.ts
+++ aprender-angular/src/app/portfolio/portfolio.component.ts
@@ -8,4 +8,22 @@ import { Component } from '@angular/core';
 })
 export class PortfolioComponent {
   web = "https://portfolioantonioheredia.netlify.app/"
+  redirigir = false;
+
+  lenguajes = [
+    {id:1, nombre:"Html"},
+    {id:2, nombre:"CSS"},
+    {id:3, nombre:"Javascript"},
+    {id:4, nombre:"Python"}
+  ];
+
+
+  cambiarRedirigir(){
+    if(this.redirigir == false){
+      this.redirigir =true;
+    }else{
+      this.redirigir = false;
+    }
+  }
+  
 }
