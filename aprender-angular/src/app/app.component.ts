f14cf42ce54c758e806da1c837d6aaa9ffb4e44d
Heran76 <heran76@gmail.com>
2025-09-08 20:02:37 +0200

    lunes-directivas

--- aprender-angular/src/app/app.component.ts
+++ aprender-angular/src/app/app.component.ts
@@ -3,9 +3,10 @@ import{VentasComponent} from './ventas/ventas.component';
 import { PortfolioComponent } from './portfolio/portfolio.component';
 import { UsuariosComponent } from "./usuarios/usuarios.component";
 import{ProductoComponent} from './producto/producto.component';
+import { DirectivasComponent } from './directivas/directivas.component';
 @Component({
   selector: 'app-root',
-  imports: [VentasComponent, PortfolioComponent, UsuariosComponent, ProductoComponent],
+  imports: [VentasComponent, PortfolioComponent, UsuariosComponent, ProductoComponent, DirectivasComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
