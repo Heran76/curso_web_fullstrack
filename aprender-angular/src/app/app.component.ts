9934cc958057e51d75e4ac8c88baeba43610f5c3
Heran76 <heran76@gmail.com>
2025-09-11 20:55:52 +0200

    Jueves-Angular

--- aprender-angular/src/app/app.component.ts
+++ aprender-angular/src/app/app.component.ts
@@ -4,6 +4,7 @@ import { PortfolioComponent } from './components/portfolio/portfolio.component';
 import { UsuariosComponent } from "./components/usuarios/usuarios.component";
 import{ProductoComponent} from './components/producto/producto.component';
 import { DirectivasComponent } from './components/directivas/directivas.component';
+import{ config } from './models/config';  
 @Component({
   selector: 'app-root',
   imports: [VentasComponent, PortfolioComponent, UsuariosComponent, ProductoComponent, DirectivasComponent],
@@ -12,4 +13,6 @@ import { DirectivasComponent } from './components/directivas/directivas.componen
 })
 export class AppComponent {
   title = 'Primer proyecto con angular';
+  titulo = config.title;
+  descripcion = config.descripcion
 }
---
+++ aprender-angular/src/app/models/config.ts
@@ -0,0 +1,8 @@
+export const config ={
+    color: 'blue',
+    fondo: 'red',
+    theme: 'dark',
+    title: 'Aprender Angular',
+    descripcion: 'Aprendiendo Angular paso a paso',
+    url : 'www.aprenderangular.com'
+};
